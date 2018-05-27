2feed9141a5815527112e54bab36076ca42eb7ef
k7295 <klcho.o@hotmail.com>
2018-05-27 01:14:30 -0600

    lectura de XML e inicio de interfaz

---
+++ WindowsFormsApp1/WindowsFormsApp1/Funciones.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Funciones : Form
+    {
+        /// <summary>
+        /// Contains column names.
+        /// </summary>
+        List<string> _names = new List<string>();
+
+        /// <summary>
+        /// Contains column data arrays.
+        /// </summary>
+        List<double[]> _dataArray = new List<double[]>();
+
+        public Funciones()
+        {
+            InitializeComponent();
+
+            // Example column.
+            _names.Add("Cat");
+            // Three numbers of cat data.
+            _dataArray.Add(new double[]
+            {
+                1.0,
+                2.2,
+                3.4
+            });
+
+            // Another example column.
+            _names.Add("Dog");
+            // Add three numbers of dog data.
+            _dataArray.Add(new double[]
+            {
+                3.3,
+                5.0,
+                7.0
+            });
+            // Render the DataGridView.
+            dataGridView1.DataSource = GetResultsTable();
+        }
+
+        /// <summary>
+        /// This method builds a DataTable of the data.
+        /// </summary>
+        public DataTable GetResultsTable()
+        {
+            // Create the output table.
+            DataTable d = new DataTable();
+
+            // Loop through all process names.
+            for (int i = 0; i < this._dataArray.Count; i++)
+            {
+                // The current process name.
+                string name = this._names[i];
+
+                // Add the program name to our columns.
+                d.Columns.Add(name);
+
+                // Add all of the memory numbers to an object list.
+                List<object> objectNumbers = new List<object>();
+
+                // Put every column's numbers in this List.
+                foreach (double number in this._dataArray[i])
+                {
+                    objectNumbers.Add((object)number);
+                }
+
+                // Keep adding rows until we have enough.
+                while (d.Rows.Count < objectNumbers.Count)
+                {
+                    d.Rows.Add();
+                }
+
+                // Add each item to the cells in the column.
+                for (int a = 0; a < objectNumbers.Count; a++)
+                {
+                    d.Rows[a][i] = objectNumbers[a];
+                }
+            }
+            return d;
+        }
+    }
+        //    public Funciones()
+        //    {
+        //        InitializeComponent();
+        //        this.CenterToScreen();
+        //    }
+
+        //    private void logica_labels(string accion)
+        //    {   
+        //        resetOpciones();
+        //        switch (accion)
+        //        {
+        //            case "mostrar agentes":
+        //                Console.WriteLine("mostrar agentes");
+        //                picture_amarillo.Visible = true;
+        //                mostrarAgentes.Image = global::WindowsFormsApp1.Properties.Resources.amarillo;
+        //                DataTable dt = new DataTable("Northwind");
+        //                DataColumn dc = new DataColumn("ProductID", typeof(int));
+
+        //                break;
+        //            case "mostrar servicios":
+        //                Console.WriteLine("mostrar servicios");
+        //                picture_naranja.Visible = true;
+        //                mostrarServicios.Image = global::WindowsFormsApp1.Properties.Resources.naranja;
+        //                break;
+        //            case "repartir ordenes":
+        //                Console.WriteLine("repartir ordenes");
+        //                repartirOrdenes.Image = global::WindowsFormsApp1.Properties.Resources.verde;
+        //                picture_verde.Visible = true;
+        //                break;
+
+        //        }
+        //    }
+
+        //    private void resetOpciones()
+        //    {
+        //        mostrarAgentes.Image = null;
+        //        mostrarServicios.Image = null;
+        //        repartirOrdenes.Image = null;
+
+        //        picture_amarillo.Visible = false;
+        //        picture_naranja.Visible = false;
+        //        picture_verde.Visible = false;
+
+        //    }
+        //    private void boton_temporal_Click(object sender, EventArgs e)
+        //    {
+        //       string texto = this.textBox_temporal.Text;
+        //       logica_labels(texto);
+        //    }
+
+        //}
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Principal.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Titulo_GE = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_temporal = new System.Windows.Forms.TextBox();
+            this.boton_temporal = new System.Windows.Forms.Button();
+            this.label_iniciar = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picture_rojo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_rojo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Titulo_GE
+            // 
+            this.Titulo_GE.AutoSize = true;
+            this.Titulo_GE.Font = new System.Drawing.Font("Segoe Print", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titulo_GE.Location = new System.Drawing.Point(250, 33);
+            this.Titulo_GE.Name = "Titulo_GE";
+            this.Titulo_GE.Size = new System.Drawing.Size(322, 61);
+            this.Titulo_GE.TabIndex = 0;
+            this.Titulo_GE.Text = "General Electric ";
+            this.Titulo_GE.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(125, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(556, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Aplicación para distribución de servicios";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(326, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Escucha con atención las instrucciones";
+            // 
+            // textBox_temporal
+            // 
+            this.textBox_temporal.Location = new System.Drawing.Point(310, 248);
+            this.textBox_temporal.Name = "textBox_temporal";
+            this.textBox_temporal.Size = new System.Drawing.Size(185, 20);
+            this.textBox_temporal.TabIndex = 5;
+            // 
+            // boton_temporal
+            // 
+            this.boton_temporal.Location = new System.Drawing.Point(509, 246);
+            this.boton_temporal.Name = "boton_temporal";
+            this.boton_temporal.Size = new System.Drawing.Size(99, 21);
+            this.boton_temporal.TabIndex = 6;
+            this.boton_temporal.Text = "button1";
+            this.boton_temporal.UseVisualStyleBackColor = true;
+            this.boton_temporal.Click += new System.EventHandler(this.boton_temporal_Click);
+            // 
+            // label_iniciar
+            // 
+            this.label_iniciar.AutoSize = true;
+            this.label_iniciar.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_iniciar.Location = new System.Drawing.Point(366, 327);
+            this.label_iniciar.Name = "label_iniciar";
+            this.label_iniciar.Size = new System.Drawing.Size(71, 23);
+            this.label_iniciar.TabIndex = 4;
+            this.label_iniciar.Text = "Iniciar";
+            this.label_iniciar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.audio_icon3;
+            this.pictureBox1.Location = new System.Drawing.Point(356, 202);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(38, 26);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // picture_rojo
+            // 
+            this.picture_rojo.Image = global::WindowsFormsApp1.Properties.Resources.rojo1;
+            this.picture_rojo.Location = new System.Drawing.Point(328, 274);
+            this.picture_rojo.Name = "picture_rojo";
+            this.picture_rojo.Size = new System.Drawing.Size(145, 132);
+            this.picture_rojo.TabIndex = 7;
+            this.picture_rojo.TabStop = false;
+            this.picture_rojo.Visible = false;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(839, 426);
+            this.Controls.Add(this.boton_temporal);
+            this.Controls.Add(this.textBox_temporal);
+            this.Controls.Add(this.label_iniciar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Titulo_GE);
+            this.Controls.Add(this.picture_rojo);
+            this.Name = "Principal";
+            this.Text = "General Electric";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_rojo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Titulo_GE;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label_iniciar;
+        private System.Windows.Forms.TextBox textBox_temporal;
+        private System.Windows.Forms.Button boton_temporal;
+        private System.Windows.Forms.PictureBox picture_rojo;
+    }
+}
+
